4322b591993ebb56b805e95aeb4b54f0e8d59877
Steven Delgado <threzstv@gmail.com>
2015-12-25 15:55:52 -0300

    delte entity

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,18 @@ class ArticlesController < ApplicationController
 
 
 	def show
+
 		@article = Article.find(params[:id])
+
+	end
+
+	def destroy
+
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to root_path
+
 	end
 
 
